e05fc359050b120b95c6e2583c8433cc92252ff7
GabrielCavalcanti13 <gcm2@cin.ufpe.br>
2021-06-22 22:40:35 -0300

    conecting project to firebase

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,6 +1,11 @@
+import {Button} from './components/Button'
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Button text='Botao 1'/>
+      <Button text='Botao 2'/>
+    </div>
   );
 }
 
